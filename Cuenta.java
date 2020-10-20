4af0a06494c12a2a9b3c9ade9012b337ccbe5eaa
MotzoFranco <francomotzo2000@gmail.com>
2020-10-20 19:21:29 -0300

    Cuenta con Main

---
+++ Cuenta.java
@@ -0,0 +1,25 @@
+
+public class Cuenta {
+	private String email;
+	private String contrasena;
+
+   public Cuenta(String email, String contrasena) {
+	   this.email = email;
+	   this.contrasena = email;
+   }
+
+   public String getEmail() {
+	   return this.email;
+   }
+   
+   public String getContrasena() {
+	   return this.contrasena;
+   }
+   
+   public boolean isValid(String email, String contrasena) {
+
+	   return email.equals(this.email) && contrasena.equals(this.contrasena);
+   
+   }
+
+}
